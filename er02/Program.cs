cbf9749f60b0347275e4bcd27d5de6666a7b3d58
LucasKJR <lucaskalencastro@gmail.com>
2023-05-15 23:55:09 -0300

    Atualizado ate ER3

--- er02/Program.cs
+++ er02/Program.cs
@@ -4,6 +4,8 @@ PessoaFisica novaPF = new PessoaFisica();
 
 novaPF.nome = "lucas";
 novaPF.cpf = "123456789";
+novaPF.rendimento = 6600.5f;
+
 
 // Console.WriteLine(novaPF.nome);
 // Console.WriteLine(novaPF.cpf);
@@ -14,5 +16,13 @@ Console.WriteLine("Bem vindo - " + novaPF.nome);
 //interpolação
 Console.WriteLine($"Bem vindo: {novaPF.nome} de Cpf: {novaPF.cpf}");
 
+// Pessoa Fisica =======================================================================
+float impostaPagar = novaPF.CalcularImposto (novaPF.rendimento);
+Console.WriteLine($"{impostaPagar:0.00}");
+Console.WriteLine(impostaPagar.ToString("C"));
+
+// Pessoa Juridica =========================================================================
+PessoaJuridica novaPj = new PessoaJuridica();
+Console.WriteLine(novaPj.CalcularImposto(6600.5f));
 
 
